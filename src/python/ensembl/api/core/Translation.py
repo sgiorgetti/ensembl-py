6a78adb453766c0568b221d67c964af290e92f52
Stefano Giorgetti <sgiorgetti@ebi.ac.uk>
2024-03-20 11:02:00 +0000

    fixed pylint complain

--- src/python/ensembl/api/core/Translation.py
+++ src/python/ensembl/api/core/Translation.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Union
 from . import SplicedExon
 from .Strand import Strand
-from typing import Union
 
 
 __all__ = [ 'Translation' ]
